d471ff4f0cdcb7167f3f0c36c05d0e0132587b2a
Josh Jaffe <josjaf@gmail.com>
2021-12-10 10:39:12 -0500

    switching to local time zone

--- outagedetector/log_f.py
+++ outagedetector/log_f.py
@@ -3,8 +3,8 @@ from pathlib import Path
 def separate_log_file(current_timestamp, internet_connected, just_booted):
     now = current_timestamp.now(pytz.timezone('America/New_York'))
 
-    file_name = current_timestamp.strftime("%Y-%m-%d")
-    log_line_date = current_timestamp.strftime("%H:%M:%S")
+    file_name = now.strftime("%Y-%m-%d")
+    log_line_date = now.strftime("%H:%M:%S")
     l = f"Script has run at {log_line_date}. Internet connected: {internet_connected}. Just booted: {just_booted}.\n"
     file = Path.home().joinpath(f'.config/outagedetector/{file_name}.log')
     # print(f"Writing to {file}")
