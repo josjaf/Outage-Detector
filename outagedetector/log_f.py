217d5a868a18f5a35c5d53a9c8368d21bb12328d
Josh Jaffe <josjaf@gmail.com>
2021-12-17 14:04:12 -0500

    adding ping time to message

--- outagedetector/log_f.py
+++ outagedetector/log_f.py
@@ -1,6 +1,6 @@
 import pytz
 from pathlib import Path
-def separate_log_file(current_timestamp, internet_connected, just_booted):
+def separate_log_file(current_timestamp, internet_connected, just_booted, ping_time):
     if just_booted:
         just_booted_message = "YES"
     else:
@@ -9,7 +9,7 @@ def separate_log_file(current_timestamp, internet_connected, just_booted):
 
     file_name = now.strftime("%Y-%m-%d")
     log_line_date = now.strftime("%H:%M:%S")
-    l = f"Script has run at {log_line_date}. Internet connected: {internet_connected}. Just booted: {just_booted_message}.\n"
+    l = f"Script has run at {log_line_date}. Internet connected: {internet_connected}. Just booted: {just_booted_message}. Ping Time: {ping_time}\n"
     file = Path.home().joinpath(f'.config/outagedetector/{file_name}.log')
     # print(f"Writing to {file}")
     with open(file, 'a+') as f:
